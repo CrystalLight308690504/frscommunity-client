1237fda4748d1bfcb2d71bec7406948c4dac435f
朱效 <308690504@qq.com>
2022-01-02 11:19:55 +0800

    加入butterknife

--- app/src/main/java/com/crystallightghot/frscommunityclient/contract/HomeContract.java
+++ app/src/main/java/com/crystallightghot/frscommunityclient/contract/HomeContract.java
@@ -1,22 +1,30 @@
 package com.crystallightghot.frscommunityclient.contract;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.crystallightghot.frscommunityclient.R;
 import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 
 /**
- * @author  crystallight
+ * @author crystallight
  */
 public class HomeContract extends BaseContract {
+
+    @BindView(R.id.home_top_bar_background)
+    TextView homeTopBarBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        homeTopBarBackground.setHeight(QMUIStatusBarHelper.getStatusbarHeight(this));
     }
 }
